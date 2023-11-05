c6916ffc13331cb3efa30be0830df272898e5321
BabiPunSociu <nguyendung281002@gmail.com>
2023-11-05 15:07:49 +0700

    wip: hoàn thiện trang hoctap

--- public/client/khoa_hoc.php
+++ public/client/khoa_hoc.php
@@ -1,17 +1,26 @@
 <?php
+require_once __DIR__ . "/../../configs/config.php";
+require_once __DIR__ . "/../../configs/function.php";
 $title = 'Danh sách khóa học | ' . $Database->site("TenWeb") . '';
 $locationPage = 'khoahoc';
+$META_TITLE = "Danh sách khóa học";
 $META_IMAGE = "https://i.imgur.com/WlasPkc.png";
+$META_DESCRIPTION = "Danh sách khóa học";
 $META_SITE = BASE_URL("Page/KhoaHoc");
+require_once __DIR__ . "/../../public/client/header.php";
+checkLogin();
 ?>
 <style>
-   
+    <?=include_once __DIR__ . "/../../assets/css/course_page.css";
+?>
 </style>
 
 <div class="grid">
     <div class="row main-page">
         <div class="nav-container">
-           
+            <?php
+include_once __DIR__ . "/../../public/client/navigation.php";
+?>
         </div>
         <div class="main_content-container">
             <div class="list-course">
@@ -19,55 +28,55 @@ $META_SITE = BASE_URL("Page/KhoaHoc");
                     <div class="page__title">Danh sách các khóa học:</div>
                     <div class="list_course-detail__content-container">
                         <?php
-                        foreach ($Database->get_list(" SELECT * FROM khoahoc ") as $row) {
-                            $checkDangKy = $Database->get_row("SELECT * FROM dangkykhoahoc WHERE `TaiKhoan` = '" . $_SESSION["account"] . "' AND `MaKhoaHoc` = '" . $row["MaKhoaHoc"] . "' ");
-                            $soHocVien = $Database->num_rows("SELECT * FROM dangkykhoahoc WHERE MaKhoaHoc = '" . $row["MaKhoaHoc"] . "'  ");
-                        ?>
+foreach ($Database->get_list(" SELECT * FROM khoahoc ") as $row) {
+    $checkDangKy = $Database->get_row("SELECT * FROM dangkykhoahoc WHERE `TaiKhoan` = '" . $_SESSION["account"] . "' AND `MaKhoaHoc` = '" . $row["MaKhoaHoc"] . "' ");
+    $soHocVien = $Database->num_rows("SELECT * FROM dangkykhoahoc WHERE MaKhoaHoc = '" . $row["MaKhoaHoc"] . "'  ");
+    ?>
                             <div class="list-course__plan card">
-                                <img src=<?= $row["LinkAnh"] ?> alt="" class="list-course__plan-img js-english__img">
+                                <img src=<?=$row["LinkAnh"]?> alt="" class="list-course__plan-img js-english__img">
                                 <div class="list-course__plan-content">
-                                    <div class="list-course__plan-heading"><a href="<?= BASE_URL('Page/KhoaHoc/' . $row["MaKhoaHoc"] . '') ?>"><?= $row["TenKhoaHoc"] ?></a></div>
+                                    <div class="list-course__plan-heading"><a href="<?=BASE_URL('Page/KhoaHoc/' . $row["MaKhoaHoc"] . '')?>"><?=$row["TenKhoaHoc"]?></a></div>
                                     <div class="list-course__plan-tick">
-                                        <img src="<?= BASE_URL("/") ?>/assets/img/practice.svg" alt="" class="list-course__plan-tick-img">
-                                        <span class="list-course__plan-tick-number"><?= $soHocVien ?> học viên</span>
+                                        <img src="<?=BASE_URL("/")?>/assets/img/practice.svg" alt="" class="list-course__plan-tick-img">
+                                        <span class="list-course__plan-tick-number"><?=$soHocVien?> học viên</span>
                                     </div>
                                     <?php
-                                    if ($checkDangKy > 0) {
-                                    ?>
+if ($checkDangKy > 0) {
+        ?>
                                         <div class="list-course__plan-btn btn btn--no_active list-course__plan-btn--no-active">Đã đăng kí</div>
                                     <?php
-                                    } else {
-                                    ?>
-                                        <div class="modal" id="modal-register-course-<?= $row["MaKhoaHoc"] ?>">
+} else {
+        ?>
+                                        <div class="modal" id="modal-register-course-<?=$row["MaKhoaHoc"]?>">
                                             <div class="modal-background"></div>
                                             <div class="modal-content">
                                                 <div class="modal-content-body">
                                                     <div class="modal-header__text">
-                                                        Xác nhận đăng ký khóa học <?= $row["TenKhoaHoc"] ?> </div>
+                                                        Xác nhận đăng ký khóa học <?=$row["TenKhoaHoc"]?> </div>
                                                     <div class="modal-close modal-close-btn" aria-label="close">
                                                     </div>
                                                     <div class="modal-content-body__text">
                                                         Bạn muốn đăng ký khóa học này không?
                                                     </div>
 
-                                                    <div class="btn btn--primary" onclick='handleRegisterCourse(<?= $row["MaKhoaHoc"] ?>)'>
+                                                    <div class="btn btn--primary" onclick='handleRegisterCourse(<?=$row["MaKhoaHoc"]?>)'>
                                                         Xác nhận
                                                     </div>
                                                 </div>
 
                                             </div>
                                         </div>
-                                        <div class="list-course__plan-btn btn js-modal-trigger" data-target="modal-register-course-<?= $row["MaKhoaHoc"] ?>">Đăng kí</div>
+                                        <div class="list-course__plan-btn btn js-modal-trigger" data-target="modal-register-course-<?=$row["MaKhoaHoc"]?>">Đăng kí</div>
                                     <?php
-                                    }
-                                    ?>
+}
+    ?>
 
                                 </div>
                             </div>
 
                         <?php
-                        }
-                        ?>
+}
+?>
 
                     </div>
 
@@ -79,7 +88,7 @@ $META_SITE = BASE_URL("Page/KhoaHoc");
         <script>
             function handleRegisterCourse(maKhoaHoc) {
                 $.ajax({
-                    url: "<?= BASE_URL("assets/ajaxs/Course.php"); ?>",
+                    url: "<?=BASE_URL("assets/ajaxs/Course.php");?>",
                     method: "POST",
                     data: {
                         type: 'registerCourse',
@@ -100,3 +109,9 @@ $META_SITE = BASE_URL("Page/KhoaHoc");
             }
             $(document).ready(function() {});
         </script>
+        <?php
+include_once __DIR__ . "/../../public/client/menu_right.php";
+include_once __DIR__ . "/../../public/client/navigation_mobile.php";
+?>
+        <?php
+require_once __DIR__ . "/../../public/client/footer.php";?>
