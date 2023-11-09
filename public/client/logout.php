f14d08396882afa6e01afd4bd29ea11a91f23415
XXMISSILEXX <89600178+XXMISSILEXX@users.noreply.github.com>
2023-11-09 23:43:22 +0700

    Delete logout.php

--- public/client/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__ . "/../../configs/config.php");
-require_once(__DIR__ . "/../../configs/function.php");
-
-
-session_destroy();
-header('location: ' . BASE_URL("/"));
-
-?>
